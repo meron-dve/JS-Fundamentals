91f7cac9aafd9e32288925d017784ef88431720e
meron <merontty1@gmail.com>
2025-11-08 15:39:20 +0300

    using arguments

---
+++ 2-arguments.js
@@ -0,0 +1,13 @@
+function greet(name) {
+console.log ("No argument");
+}
+greet ();
+function gret(nam) {
+    console.log("Argument" + " " + nam);
+
+}
+gret("found");
+function fun(yy) {
+    console.log ("Arguments found")
+}
+fun();
