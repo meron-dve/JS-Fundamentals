73ea55d0b70a8bb51334563f252fe189991dd0e2
meron <merontty1@gmail.com>
2025-11-09 12:07:24 +0300

    arguments

---
+++ 4-concat.js
@@ -0,0 +1,13 @@
+function nan(nu,me) {
+    console.log(nu + " " + me);
+
+}
+nan("c is","cool");
+function get(nb,nj) {
+    console.log(nb + " " + nj)
+}
+get("c is","undefined");
+function cpu(mer,hen) {
+    console.log(mer + " " + hen);
+}
+cpu("undefined is","undefined");
