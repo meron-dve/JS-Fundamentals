b2d38e8b2029f463bd244bec78b4583d1be88b0d
meron <merontty1@gmail.com>
2025-11-09 12:51:45 +0300

    integer

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,3 +2,12 @@ function bb(nb) {
     console.log("My number:" + " " + nb);
 }
 bb("89");
+function xx(ii) {
+    console.log("My number:" + " " + ii);
+
+}
+xx("-4545");
+function jj(uu) {
+    console.log("Not a number")
+}
+jj();
