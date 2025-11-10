54fa7fde94aee83953d72a924b8afe91a63f379c
meron <merontty1@gmail.com>
2025-11-10 19:09:05 +0300

    ppp

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,13 +1,13 @@
-function greet(name) {
-console.log ("No argument");
-}
-greet ();
-function gret(nam) {
-    console.log("Argument" + " " + nam);
+#!/usr/bin/node
 
+// Count how many arguments are passed (excluding the first two: node and script path)
+const count = process.argv.length - 2;
+
+// Print message based on number of arguments
+if (count === 0) {
+  console.log('No argument');
+} else if (count === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-gret("found");
-function fun(yy,pp,nn) {
-    console.log (yy + pp + " " + nn)
-}
-fun("Argu","ments","found");
