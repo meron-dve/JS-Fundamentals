771650ba90f4a56710101f165cf5fdb503eab690
meron <merontty1@gmail.com>
2025-11-10 18:34:51 +0300

    f

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Create an array of strings
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Loop through the array and print each line
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
+
