a37bd567f93e7101659965f3ed0e4b2fd94c6a43
meron <merontty1@gmail.com>
2025-11-10 18:30:49 +0300

    i

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,13 +1,15 @@
-function bb(nb) {
-    console.log("My number:" + " " + nb);
-}
-bb("89");
-function xx(ii) {
-    console.log("My number:" + " " + ii);
+#!/usr/bin/node
 
+// Get the first command-line argument
+const arg = process.argv[2];
+
+// Convert to integer
+const num = parseInt(arg);
+
+// Check if it's a valid number
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${num}`);
 }
-xx("-4545");
-function jj(uu) {
-    console.log("Not a number")
-}
-jj();
+
